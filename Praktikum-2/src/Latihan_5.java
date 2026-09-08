e1bf6204473c4745ffc0b7a942cc86ecf26922c5
Abdul Razak <abilnew110@gmail.com>
2026-09-08 11:41:55 +0700

    memisahkan latihan 1-5

---
+++ Praktikum-2/src/Latihan_5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Latihan_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka1: ");
+        int angka1 = sc.nextInt();
+
+        System.out.print("Masukkan angka2: ");
+        int angka2 = sc.nextInt();
+
+        System.out.println("Jumlah : " + (angka1 + angka2));
+        System.out.println("Selisih : " + (angka1 - angka2));
+        System.out.println("kali : " + (angka1 * angka2));
+        System.out.println("bagi : " + (angka1 / angka2));
+        System.out.println("sisa bagi : " + (angka1 % angka2));
+
+        System.out.println(angka1 + " > " + angka2 + " : " + (angka1 > angka2));
+        System.out.println(angka1 + " < " + angka2 + " : " + (angka1 < angka2));
+        System.out.println(angka1 + " == " + angka2 + " : " + (angka1 == angka2));
+    }
+}
