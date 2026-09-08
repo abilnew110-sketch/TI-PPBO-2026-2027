a07d2e0ed4c737a74ef7a6e1f78886eb472181ce
Abdul Razak <abilnew110@gmail.com>
2026-09-08 10:48:36 +0700

    latihan nomor 2

--- Praktikum-2/src/BiodataSaya.java
+++ Praktikum-2/src/BiodataSaya.java
@@ -1,10 +1,5 @@
 public class BiodataSaya {
     public static void main(String[] args) {
-        //menampilkan nama saya
-        System.out.println("Nama saya Abdul Razak");
-        /*menampilkan nim*/
-        System.out.println("NIM saya 2025573010068");
-        //menampilkan Prodi saya
-        System.out.println("Program Studi saya adalah Teknik Informatika");
+        System.out.print("nama saya Abdul Razak - 2025573010068");
     }
 }
